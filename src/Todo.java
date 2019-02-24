4948346983bf686ab852476523d0a3dc86c6937e
Nathan Preuss <nathan.b.preuss-1@ou.edu>
2019-02-24 17:44:16 -0600

    first commit initialize repository

---
+++ src/Todo.java
@@ -0,0 +1,28 @@
+package lab6.src;
+public class Todo {/*
+Lab Section_________: 012
+Last Modified_______: 2019-02-14
+OU ID_______________: 113411201
+OU EMAIL____________: nathan.b.preuss-1@ou.edu
+
+Tasks:
+1. implement calculateTwoTokens
+2. implement calculateThreeTokens
+3. implement execute
+4. implement parseAndExecute
+5. implement calculatorTest
+
+Estimated Time (minutes):
+1. 10
+2. 15
+3. 10
+4. 15
+5. 30
+
+Actual Time (minutes):
+1. 5
+2. 8
+3. 11
+4. 20
+5. 40
+*/}
